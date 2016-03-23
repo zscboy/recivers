65b075bce3ea238da438b8550f216589aba9dd52
riguang <zscboy@gmail.com>
2016-03-23 10:43:41 +0800

    version and help

--- main.go
+++ main.go
@@ -12,8 +12,19 @@ import (
 
 func main() {
 	cfg := flag.String("c", "cfg.json", "configuration file")
+	version := flag.Bool("v", false, "show version")
+	help := flag.Bool("h", false, "help")
 	flag.Parse()
 
+	if *version {
+		fmt.Println(g.VERSION)
+		os.Exit(0)
+	}
+
+	if *help {
+		flag.Usage()
+		os.Exit(0)
+	}
 
 	g.ParseConfig(*cfg)
 	go http.Start()
